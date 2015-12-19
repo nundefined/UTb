7bc9fb687abb68f3d2dfe702f05b3a402159c12c
Jeonghwan Jang <nundefined@nundefined.com>
2015-12-19 22:31:30 +0900

    Delete keys.js

--- keys.js
+++
@@ -1,5 +0,0 @@
-var keys = {
-  apiKey: 'AIzaSyCLFHwRbVpFgxzd0HEYTNxg0lxFxcrh0xU',
-  clientId: '108335246822-2tej4gek1c7ojejkajbifmg0lvg3d37h.apps.googleusercontent.com',
-  scopes: 'https://www.googleapis.com/auth/youtube'
-};
