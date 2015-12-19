1ebb49c4bd5546338c27247333fd1cba84b56b03
Jeonghwan Jang <nundefined@nundefined.com>
2015-12-19 22:28:06 +0900

    Applied a new look.

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 var keys = {
   apiKey: 'AIzaSyCLFHwRbVpFgxzd0HEYTNxg0lxFxcrh0xU',
-  clientId: '108335246822-b9lil47e4b9njmr5odmucmatp8dt8876.apps.googleusercontent.com',
+  clientId: '108335246822-2tej4gek1c7ojejkajbifmg0lvg3d37h.apps.googleusercontent.com',
   scopes: 'https://www.googleapis.com/auth/youtube'
 };
