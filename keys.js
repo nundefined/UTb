1510923db6a9c2fd57d31b1e78f7678de5b62812
Jeonghwan Jang <nundefined@nundefined.com>
2016-01-02 00:32:27 +0900

    create keys.js for github pages

---
+++ keys.js
@@ -0,0 +1,5 @@
+ var keys = {
+  apiKey: 'AIzaSyCLFHwRbVpFgxzd0HEYTNxg0lxFxcrh0xU',
+  clientId: '108335246822-b9lil47e4b9njmr5odmucmatp8dt8876.apps.googleusercontent.com',
+  scopes: 'https://www.googleapis.com/auth/youtube'
+ };
